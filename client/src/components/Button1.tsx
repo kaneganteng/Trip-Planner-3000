485d9928fc0df2ce423d4fe4a33efa142330d374
kaneganteng <kaneesasta@gmail.com>
2024-11-05 15:45:25 -0500

    added button1.tsx

---
+++ client/src/components/Button1.tsx
@@ -0,0 +1,40 @@
+type ButtonProps = {
+    onResults: (data: any) => void;
+  };
+  
+  const ApiSearchButton: React.FC<ButtonProps> = ({ onResults }) => {
+    const handleClick = async () => {
+      const data = await searchAPI();
+      onResults(data);
+    };
+  
+  const searchAPI = async () => {
+    try {
+      const response = await fetch(
+        `https://some.random.apicom/`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+          },
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error('Invalid API response, check the network tab');
+      }
+      console.log(data)
+      return data;
+    } catch (err) {
+      console.error('An error occurred:', err);
+      return [];
+    }
+  };
+  
+    return (
+      <button onClick={handleClick}>
+        Search API
+      </button>
+    );
+  };
+  
+  export default ApiSearchButton;
